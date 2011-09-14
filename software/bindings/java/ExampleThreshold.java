39994bc79bcd0626577980e6d2200de68e1d3da8
Olaf Lüke <olaf@tinkerforge.com>
2011-09-14 16:48:18 +0200

    limit line length of java examples to 100 characters

--- software/bindings/java/ExampleThreshold.java
+++ software/bindings/java/ExampleThreshold.java
@@ -9,21 +9,24 @@ public class ExampleThreshold {
 	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
 	//       might normally want to catch are described in the commnents below
 	public static void main(String args[]) throws Exception {
-		IPConnection ipcon = new IPConnection(host, port); // Create connection to brickd (Can throw IOException)
+		// Create connection to brickd
+		IPConnection ipcon = new IPConnection(host, port); // Can throw IOException
 
-		BrickletCurrent25 al = new BrickletCurrent25(UID); // Create device object
-		ipcon.addDevice(al); // Add device to ip connection (Can throw IPConnection.TimeoutException)
+		BrickletCurrent25 c25 = new BrickletCurrent25(UID); // Create device object
+
+		// Add device to ip connection
+		ipcon.addDevice(c25); // Can throw IPConnection.TimeoutException
 		// Don't use device before it is added to a connection
 		
 
 		// Get threshold callbacks with a debounce time of 10 seconds (10000ms)
-		al.setDebouncePeriod(10000);
+		c25.setDebouncePeriod(10000);
 
 		// Configure threshold for "greater than 5A" (unit is mA)
-		al.setCurrentCallbackThreshold('>', (short)(5*1000), (short)0);
+		c25.setCurrentCallbackThreshold('>', (short)(5*1000), (short)0);
 
 		// Add and implement current reached listener (called if current is greater than 5A)
-		al.addListener(new BrickletCurrent25.CurrentReachedListener() {
+		c25.addListener(new BrickletCurrent25.CurrentReachedListener() {
 			public void currentReached(short current) {
 				System.out.println("Current is greater than 5A: " + current/1000.0);
 			}
