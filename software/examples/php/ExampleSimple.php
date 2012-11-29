ea9270d74584d396c5793f7d72b2a155089989d5
Olaf Lüke <olaf@tinkerforge.com>
2012-11-29 17:29:01 +0100

    Make PHP examples Protocol 2.0 compatible

--- software/examples/php/ExampleSimple.php
+++ software/examples/php/ExampleSimple.php
@@ -10,11 +10,11 @@ $host = 'localhost';
 $port = 4223;
 $uid = '7tS'; // Change to your UID
 
-$ipcon = new IPConnection($host, $port); // Create IP connection to brickd
-$c = new BrickletCurrent25($uid); // Create device object
+$ipcon = new IPConnection(); // Create IP connection
+$c = new BrickletCurrent25($uid, $ipcon); // Create device object
 
-$ipcon->addDevice($c); // Add device to IP connection
-// Don't use device before it is added to a connection
+$ipcon->connect($host, $port); // Connect to brickd
+// Don't use device before ipcon is connected
 
 // Get current current (unit is mA)
 $current = $c->getCurrent() / 1000.0;
@@ -23,6 +23,5 @@ echo "Current: $current A\n";
 
 echo "Press key to exit\n";
 fgetc(fopen('php://stdin', 'r'));
-$ipcon->destroy();
 
 ?>
