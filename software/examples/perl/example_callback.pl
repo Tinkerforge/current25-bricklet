495595c63a64afcf4c2ac6f00a3b2ed2bf28a9c1
Matthias Bolte <matthias@tinkerforge.com>
2015-08-19 15:41:31 +0200

    Cleanup Perl examples

--- software/examples/perl/example_callback.pl
+++ software/examples/perl/example_callback.pl
@@ -5,29 +5,29 @@ use Tinkerforge::BrickletCurrent25;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => 'abd2'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
-my $c25 = Tinkerforge::BrickletCurrent25->new(&UID, $ipcon); # Create device object
+my $c = Tinkerforge::BrickletCurrent25->new(&UID, $ipcon); # Create device object
 
-# Callback function for current callback (parameter has unit mA)
+# Callback subroutine for current callback (parameter has unit mA)
 sub cb_current
 {
     my ($current) = @_;
 
-    print "Current: ".$current/1000.0." A\n";
+    print "Current: " . $current/1000.0 . " A\n";
 }
 
 $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
 
-# Set Period for current callback to 1s (1000ms)
-# Note: The callback is only called every second if the
-#       current has changed since the last call!
-$c25->set_current_callback_period(1000);
+# Set period for current callback to 1s (1000ms)
+# Note: The current callback is only called every second
+#       if the current has changed since the last call!
+$c->set_current_callback_period(1000);
 
-# Register current callback to function cb_current
-$c25->register_callback($c25->CALLBACK_CURRENT, 'cb_current');
+# Register current callback to subroutine cb_current
+$c->register_callback($c->CALLBACK_CURRENT, 'cb_current');
 
 print "Press any key to exit...\n";
 <STDIN>;
--- software/examples/perl/example_simple.pl
+++ software/examples/perl/example_simple.pl
@@ -5,18 +5,17 @@ use Tinkerforge::BrickletCurrent25;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => 'abd2'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
-my $c25 = Tinkerforge::BrickletCurrent25->new(&UID, $ipcon); # Create device object
+my $c = Tinkerforge::BrickletCurrent25->new(&UID, $ipcon); # Create device object
 
 $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
 
 # Get current current (unit is mA)
-my $current = $c25->get_current()/1000.0;
-
-print "Current: $current A\n";
+my $current = $c->get_current();
+print "Current: " . $current/1000.0 . " A\n";
 
 print "Press any key to exit...\n";
 <STDIN>;
