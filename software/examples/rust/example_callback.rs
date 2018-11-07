7782b65217ad1865fbf0e67e455d909cfc6094a6
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:19:51 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,31 +1,32 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{current25_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{current25_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Current25 Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Current25 Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let current25_bricklet = Current25Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let c = Current25Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    //Create listener for current events.
-    let current_listener = current25_bricklet.get_current_receiver();
-    // Spawn thread to handle received events. This thread ends when the current25_bricklet
+    // Create receiver for current events.
+    let current_receiver = c.get_current_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `c` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in current_listener {
-            println!("Current: {}{}", event as f32 / 1000.0, " A");
+        for current in current_receiver {
+            println!("Current: {} A", current as f32 / 1000.0);
         }
     });
 
-    // Set period for current listener to 1s (1000ms)
+    // Set period for current receiver to 1s (1000ms).
     // Note: The current callback is only called every second
     //       if the current has changed since the last call!
-    current25_bricklet.set_current_callback_period(1000);
+    c.set_current_callback_period(1000);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
