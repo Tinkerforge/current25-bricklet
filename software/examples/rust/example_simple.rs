84491b1b73e3c518c0a89aa539075ba807bad9d8
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:52:11 +0100

    rust: Reformat examples.

--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,8 +1,6 @@
-use std::{io, error::Error};
+use std::{error::Error, io};
 
-use tinkerforge::{ip_connection::IpConnection, 
-                  current25_bricklet::*};
-
+use tinkerforge::{current25_bricklet::*, ip_connection::IpConnection};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,11 +11,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     let c = Current25Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-		// Get current current.
-let current = c.get_current().recv()?;
-		println!("Current: {} A", current as f32 /1000.0);
+    // Get current current.
+    let current = c.get_current().recv()?;
+    println!("Current: {} A", current as f32 / 1000.0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -1,8 +1,5 @@
-use std::{io, error::Error};
-use std::thread;
-use tinkerforge::{ip_connection::IpConnection, 
-                  current25_bricklet::*};
-
+use std::{error::Error, io, thread};
+use tinkerforge::{current25_bricklet::*, ip_connection::IpConnection};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,24 +10,24 @@ fn main() -> Result<(), Box<dyn Error>> {
     let c = Current25Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-		// Get threshold receivers with a debounce time of 10 seconds (10000ms).
-		c.set_debounce_period(10000);
+    // Get threshold receivers with a debounce time of 10 seconds (10000ms).
+    c.set_debounce_period(10000);
 
-     let current_reached_receiver = c.get_current_reached_callback_receiver();
+    let current_reached_receiver = c.get_current_reached_callback_receiver();
 
-        // Spawn thread to handle received callback messages. 
-        // This thread ends when the `c` object
-        // is dropped, so there is no need for manual cleanup.
-        thread::spawn(move || {
-            for current_reached in current_reached_receiver {           
-                		println!("Current: {} A", current_reached as f32 /1000.0);
-            }
-        });
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `c` object
+    // is dropped, so there is no need for manual cleanup.
+    thread::spawn(move || {
+        for current_reached in current_reached_receiver {
+            println!("Current: {} A", current_reached as f32 / 1000.0);
+        }
+    });
 
-		// Configure threshold for current "greater than 5 A".
-		c.set_current_callback_threshold('>', 5*1000, 0);
+    // Configure threshold for current "greater than 5 A".
+    c.set_current_callback_threshold('>', 5 * 1000, 0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
